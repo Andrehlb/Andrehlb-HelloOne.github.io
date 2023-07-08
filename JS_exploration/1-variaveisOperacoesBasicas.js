6ce4037586279d8050247b7876254958ad7362cb
Andrehlb <andrehlb@me.com>
2023-07-08 06:04:46 -0300

    update Barbearia Andrehlb project

--- JS_exploration/1-variaveisOperacoesBasicas.js
+++ JS_exploration/1-variaveisOperacoesBasicas.js
@@ -1,12 +1,16 @@
-console.log("Meu Primeiro programa: trabalhando com variáveis");
+console.log("Variáveis e Operações Básicas:");
 
 const idade = 26;
 
 console.log("idade");
 console.log(idade);
+console.log("Soma da idade+2");
 console.log(idade+2);
+console.log("Subtração da idade-2");
 console.log(idade-2);
+console.log("Divisão da idade*2");
 console.log(idade/2);
 
+console.log("Atribuição de valor +2 a variável idadeSomada");
 const idadeSomada = idade+2;
 console.log(idadeSomada);
